2b9124092beb2849840716ba91b59c2646f29310
Cindy <cindyakumu40@gmail.com>
2024-07-13 00:00:54 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to set up the event listener
+  addingEventListener();
+  input.addEventListener('click', clickAlert); // Correct
+input.addEventListener('click', clickAlert()); // Incorrect
+
